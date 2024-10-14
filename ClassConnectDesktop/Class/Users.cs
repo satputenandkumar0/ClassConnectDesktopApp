50fbbeeb60cfd45f0b934cdb44d19c360777e503
satputenandkumar0 <43993495+satputenandkumar0@users.noreply.github.com>
2024-10-14 12:09:56 +0530

    Add files via upload

---
+++ ClassConnectDesktop/Class/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassConnectDesktop.Class
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FirebaseAuthSecret { get; set; }
+        public string FirebaseUrl { get; set; }
+        public string Email { get; set; }
+        public string EmailPassword { get; set; }
+    }
+}
