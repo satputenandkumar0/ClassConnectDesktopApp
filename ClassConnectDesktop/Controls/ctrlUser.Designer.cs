f0199a521dc94e9a26ed7a790182100376de0d05
satputenandkumar0 <43993495+satputenandkumar0@users.noreply.github.com>
2024-10-14 12:10:34 +0530

    Add files via upload

---
+++ ClassConnectDesktop/Controls/ctrlUser.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ClassConnectDesktop
+{
+    partial class ctrlUser
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.UserId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Login = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Password = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirebaseAuthSecret = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirebaseUrl = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EmailPassword = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Update = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 118F));
+            this.tableLayoutPanel1.Controls.Add(this.dgvUsers, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnAddUser, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(998, 529);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.AllowUserToAddRows = false;
+            this.dgvUsers.AllowUserToDeleteRows = false;
+            this.dgvUsers.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.dgvUsers.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.UserId,
+            this.Login,
+            this.Password,
+            this.FirstName,
+            this.LastName,
+            this.FirebaseAuthSecret,
+            this.FirebaseUrl,
+            this.Email,
+            this.EmailPassword,
+            this.Update});
+            this.tableLayoutPanel1.SetColumnSpan(this.dgvUsers, 2);
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvUsers.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvUsers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvUsers.Location = new System.Drawing.Point(3, 3);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.ReadOnly = true;
+            this.dgvUsers.RowTemplate.Height = 24;
+            this.dgvUsers.Size = new System.Drawing.Size(992, 479);
+            this.dgvUsers.TabIndex = 0;
+            this.dgvUsers.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUsers_CellContentClick);
+            // 
+            // UserId
+            // 
+            this.UserId.HeaderText = "UserId";
+            this.UserId.Name = "UserId";
+            this.UserId.ReadOnly = true;
+            this.UserId.Visible = false;
+            // 
+            // Login
+            // 
+            this.Login.HeaderText = "Login";
+            this.Login.Name = "Login";
+            this.Login.ReadOnly = true;
+            // 
+            // Password
+            // 
+            this.Password.HeaderText = "Password";
+            this.Password.Name = "Password";
+            this.Password.ReadOnly = true;
+            // 
+            // FirstName
+            // 
+            this.FirstName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.FirstName.HeaderText = "First Name";
+            this.FirstName.Name = "FirstName";
+            this.FirstName.ReadOnly = true;
+            // 
+            // LastName
+            // 
+            this.LastName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.LastName.HeaderText = "Last Name";
+            this.LastName.Name = "LastName";
+            this.LastName.ReadOnly = true;
+            // 
+            // FirebaseAuthSecret
+            // 
+            this.FirebaseAuthSecret.HeaderText = "Firebase AutSecret";
+            this.FirebaseAuthSecret.Name = "FirebaseAuthSecret";
+            this.FirebaseAuthSecret.ReadOnly = true;
+            // 
+            // FirebaseUrl
+            // 
+            this.FirebaseUrl.HeaderText = "Firebase Url";
+            this.FirebaseUrl.Name = "FirebaseUrl";
+            this.FirebaseUrl.ReadOnly = true;
+            // 
+            // Email
+            // 
+            this.Email.HeaderText = "Email";
+            this.Email.Name = "Email";
+            this.Email.ReadOnly = true;
+            // 
+            // EmailPassword
+            // 
+            this.EmailPassword.HeaderText = "Email Password";
+            this.EmailPassword.Name = "EmailPassword";
+            this.EmailPassword.ReadOnly = true;
+            // 
+            // Update
+            // 
+            this.Update.HeaderText = "";
+            this.Update.Name = "Update";
+            this.Update.ReadOnly = true;
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAddUser.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnAddUser.FlatAppearance.BorderSize = 2;
+            this.btnAddUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddUser.Location = new System.Drawing.Point(883, 488);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(112, 38);
+            this.btnAddUser.TabIndex = 1;
+            this.btnAddUser.Text = "Add User";
+            this.btnAddUser.UseVisualStyleBackColor = true;
+            this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
+            // 
+            // ctrlUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ctrlUser";
+            this.Size = new System.Drawing.Size(998, 529);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dgvUsers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Login;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Password;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirebaseAuthSecret;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirebaseUrl;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EmailPassword;
+        private System.Windows.Forms.DataGridViewButtonColumn Update;
+        private System.Windows.Forms.Button btnAddUser;
+    }
+}
---
+++ ClassConnectDesktop/Controls/ctrlUser.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassConnectDesktop.Forms;
+using FireSharp.Response;
+using ClassConnectDesktop.Class;
+
+namespace ClassConnectDesktop
+{
+    public partial class ctrlUser : UserControl
+    {
+        private Users selectedUser;
+        public ctrlUser()
+        {
+            InitializeComponent();
+            FillUserDetails();
+        }
+        private async void FillUserDetails()
+        {
+            FirebaseManager manager = new FirebaseManager();
+            FirebaseResponse responseGetAllUser = await manager.client.GetAsync("Users");
+            List<Users> lstUsers = responseGetAllUser.ResultAs<List<Users>>();
+            lstUsers.RemoveAll(x => x == null);
+            dgvUsers.Rows.Clear();
+            foreach (Users user in lstUsers)
+            {
+                dgvUsers.Rows.Add();
+                int rowIndex = dgvUsers.Rows.Count - 1;
+                dgvUsers.Rows[rowIndex].Cells[UserId.Index].Value = user.Id;
+                dgvUsers.Rows[rowIndex].Cells[Login.Index].Value = user.Login;
+                dgvUsers.Rows[rowIndex].Cells[Password.Index].Value = user.Password;
+                dgvUsers.Rows[rowIndex].Cells[FirstName.Index].Value = user.FirstName;
+                dgvUsers.Rows[rowIndex].Cells[LastName.Index].Value = user.LastName;
+                dgvUsers.Rows[rowIndex].Cells[FirebaseAuthSecret.Index].Value = user.FirebaseAuthSecret;
+                dgvUsers.Rows[rowIndex].Cells[FirebaseUrl.Index].Value = user.FirebaseUrl;
+                dgvUsers.Rows[rowIndex].Cells[Email.Index].Value = user.Email;
+                dgvUsers.Rows[rowIndex].Cells[EmailPassword.Index].Value = user.EmailPassword;
+                dgvUsers.Rows[rowIndex].Cells[Update.Index].Value = "Update";
+            }
+        }
+        private void btnAddUser_Click(object sender, EventArgs e)
+        {
+            using (frmAddUser frm = new frmAddUser())
+            {
+                if (frm.ShowDialog() == DialogResult.OK)
+                    FillUserDetails();                
+            }
+        }
+
+        private async void dgvUsers_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex == 9)
+            {
+              await  UpdateUser(e);
+                using (frmAddUser frm = new frmAddUser(selectedUser))
+                {
+                    if (frm.ShowDialog() == DialogResult.OK)
+                        FillUserDetails();
+                }
+                selectedUser = null;
+            }
+        }
+        private async Task UpdateUser(DataGridViewCellEventArgs e)
+        {
+            int userId = Convert.ToInt32(dgvUsers.Rows[e.RowIndex].Cells[UserId.Index].Value);
+            FirebaseManager manager = new FirebaseManager();
+            FirebaseResponse responseGetAllUser = await manager.client.GetAsync($"Users/{userId}");
+            selectedUser = responseGetAllUser.ResultAs<Users>();
+        }
+    }
+}
